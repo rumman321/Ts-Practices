e103fc19b63c3ecde6bef8955c792f85cd363d86
rumman321 <cset-210652@dti.ac>
2025-05-08 22:39:24 +0600

    solved First

---
+++ test.ts
@@ -0,0 +1,12 @@
+{
+  function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === false) {
+      return input.toLowerCase();
+    } else {
+      return input.toUpperCase();
+    }
+  }
+
+  const result1 = formatString("Hello");
+  console.log(result1);
+}
