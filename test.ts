62483edbbc03e01e3a8925804d9e60352dc4c187
rumman321 <cset-210652@dti.ac>
2025-05-08 23:11:14 +0600

    soled OOP problems inheritence , access modifiers

--- test.ts
+++ test.ts
@@ -60,4 +60,43 @@
   const Output1 = concatenateArrays(["a", "b"], ["c"]);
   const Output2 = concatenateArrays([1, 2], [3, 4], [5]);
 //   console.log(Output2); 
+
+
+//Problem: 4
+class Vehicle {
+    
+    private make: string;
+    private year: number;
+
+    
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    
+    public getInfo(): string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+      
+        super(make, year);
+        this.model = model;
+    }
+
+   
+    public getModel(): string {
+        return `Model: ${this.model}`;
+    }
+}
+const myCar = new Car("BMW", 2020, "i8");
+
+// console.log(myCar.getInfo());   
+// console.log(myCar.getModel());  
 }
